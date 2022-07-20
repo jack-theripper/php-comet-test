5e3ad46c3a18f7c28a232282d025d5338ca482ac
Dmitry Arhitector <dmitry.arhitector@yandex.ru>
2022-07-21 00:32:23 +0300

    show ffmpeg version

--- app/index.php
+++ app/index.php
@@ -7,3 +7,8 @@ require __DIR__ . '/../vendor/autoload.php';
 $ffmpegPath = (new ExecutableFinder())->find('ffmpeg', false);
 
 var_dump($ffmpegPath);
+
+$process = new \Symfony\Component\Process\Process([$ffmpegPath, '-version']);
+$process->mustRun();
+
+echo '<pre>'.$process->getOutput().'</pre>';
